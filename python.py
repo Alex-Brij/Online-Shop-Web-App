386726bec1c785d35f430ff369f2701c0f8ca100
Alex <brijnathad@cardiff.ac.uk>
2023-03-28 12:44:07 +0100

    initialize database

--- python.py
+++ python.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
 db = SQLAlchemy(app)
 app.app_context().push()
 
