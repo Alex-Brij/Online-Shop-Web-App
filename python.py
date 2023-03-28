7a0a0d0f95827c09584bd937752ca67a582677e2
Alex <brijnathad@cardiff.ac.uk>
2023-03-28 13:30:26 +0100

    method to add item to database

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,19 +6,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
 db = SQLAlchemy(app)
 app.app_context().push()
 
-class Item():
+# table to hold items
+class Item(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, index=True, unique=True)
     description = db.Column(db.String)
-    price = db.Column(db.Float)
+    price = db.Column(db.Integer)
     environmental_impact = db.Column(db.Integer)
+    #image = db.column(db.String)
+
+# method to add an item to the table
+
+    @staticmethod
+    def add_item(name, description, price):
+        item = Item(name=name, description=description, price=price)
+        db.session.add(item)
+        db.session.commit()
+        return item
+    
+    def __repr__(self):
+        return f'{self.name}'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('home.html', items=Item.query.all())
 
 
 if __name__ == '__main__':
     db.create_all()
+    if Item.query.filter_by(name='table').first() is None:
+        Item.add_item('table', 'wooden thing to hold plates', 100)
+    if Item.query.filter_by(name='chair').first() is None:
+        Item.add_item('chair', 'wooden thing to sit on', 50)
     app.run(debug=True)
